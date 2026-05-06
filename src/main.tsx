f88469c92fb780a0cf2e5fb75b72828e951094ed
webraftco-rgb <webraftco@gmail.com>
2026-05-06 18:25:01 +0530

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
-  </StrictMode>,
+  </StrictMode>
 );
